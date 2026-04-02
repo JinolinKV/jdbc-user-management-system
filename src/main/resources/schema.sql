442ee12fe16c2d9e86f0f581d7344fe5ca901ab2
Jinolinkv <jinolinkv@gmail.com>
2026-04-02 17:04:27 +0530

    finam fix

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 create database if not exists ums;
 
-CREATE TABLE if not exists users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    age INT(3),
-    email VARCHAR(100)
+CREATE TABLE IF NOT EXISTS users (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100),
+  age INT,
+  email VARCHAR(100)
 );
